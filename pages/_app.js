19867460933fdf438a1df517ed28bbaf6ee9255d
Jelly38214 <goolezheng@gmail.com>
2021-02-14 22:11:09 +0800

    feat: SSG and SSR.

---
+++ pages/_app.js
@@ -0,0 +1,11 @@
+/**
+ * This App component is the top-level component which will be common across all the different pages.
+ * You CANNOT import global CSS anywhere else but _app.js
+ * The reason that global CSS can't be imported outside of `pages/_app.js` is that global CSS affects all elements on the page.
+ */
+
+import "../styles/global.css";
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
