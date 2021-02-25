c5d7fa8dbdea2fdaaa3cfb7ffe1ce97ac27ad87a
Jelly38214 <goolezheng@gmail.com>
2021-02-25 21:08:44 +0800

    feat: Use cors to fix cross origin issue, and runMiddlewares fn to compose customized middlewares.

---
+++ middleware/index.js
@@ -0,0 +1,31 @@
+import { fi } from "date-fns/locale";
+
+export async function runMiddleware(req, res, ...middlewares) {
+  middlewares = middlewares.filter((fn) => typeof fn === "function");
+
+  for (const middleware of middlewares) {
+    await new Promise((resolve, reject) => {
+      middleware(req, res, (error) => {
+        error instanceof Error ? reject(error) : resolve();
+      });
+    });
+  }
+}
+
+/**
+ *
+ * @param {string[]} methods
+ * @default 'GET,HEAD,PUT,PATCH,POST,DELETE'
+ */
+export function allowRequestMethods(
+  methods = ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"]
+) {
+  return (req, res, next) => {
+    const method = req.method;
+    if (methods.includes(method)) {
+      return next();
+    }
+
+    res.status(400).end();
+  };
+}
--- pages/api/getApplePaySession.js
+++ pages/api/getApplePaySession.js
@@ -1,7 +1,12 @@
+import Cors from "cors";
+import { runMiddleware, allowRequestMethods } from "../../middleware";
+
+const cors = Cors({
+  methods: ["POST", "OPTIONS"],
+});
+
 export default async function handler(req, res) {
-  if (req.method !== "POST") {
-    return res.status(404).end();
-  }
+  await runMiddleware(req, res, cors, allowRequestMethods(["POST"]));
 
   const token = await fetch(
     "https://checkout.iherb.com/pro/getApplePaySession",
