fe2006a0fc34f1676e6591ab0c5c4a1f02d80e6c
Jelly38214 <goolezheng@gmail.com>
2021-03-02 13:56:58 +0800

    refactor: Make hello api allow cross origin.

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -2,10 +2,10 @@ import Cors from "cors";
 import { runMiddleware, allowRequestMethods } from "../../middleware";
 
 const cors = Cors({
-  methods: ["POST", "OPTIONS"],
+  methods: ["POST", "GET", "OPTIONS"],
 });
 
 export default function handler(req, res) {
-  await runMiddleware(req, res, cors, allowRequestMethods(["POST", "OPTIONS"]));
+  await runMiddleware(req, res, cors, allowRequestMethods());
   res.status(200).json({ isOk: Math.floor(Math.random() * 10) > 5 });
 }
